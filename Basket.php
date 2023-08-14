b0c5f8f922f015e30447804c3826e35e2a5f0a90
Arnaud Kameni <bekaman100@gmail.com>
2023-08-14 23:44:11 +0200

    complet basket.php

--- Basket.php
+++ Basket.php
@@ -14,16 +14,22 @@
       $pear1 = new Fruit(Fruit::PEAR, 3);
       
       $orange1 = new Fruit(Fruit::ORANGE, 10);
+      $orange2 = new Fruit(Fruit::ORANGE, 13);
   
       $grapes1 = new Fruit(Fruit::GRAPES, 5);
       $grapes2 = new Fruit(Fruit::GRAPES, 6);
 
-      $basket1 = new Basket($apple1);
-      $basket1 = new Basket($apple2);
+      $basket1 = new Basket();
+      $basket1->addFruit($apple1);
+      $basket1->addFruit($orange1);
+
+      $basket2 = new Basket();
+      $basket2->addFruit($grapes1);
+      $basket2->addFruit($orange2);
+      $basket2->addFruit($apple2);
 
       echo $basket1;
-
-
+      echo $basket2;
 ?>
 
 
--- Class/BasketClass.php
+++ Class/BasketClass.php
@@ -1,10 +1,53 @@
 <?php    
-    Class Basket{
-        private $_idBasket;
-        private $_fruits = [];
+require_once "FruitClass.php";
+    // create Basket Class.
+    Class Basket {
+        private $_basketID;
+        private $_articles = []; //Array property to Liste of article in bastket.
         private $_basketWeight;
         private $_basketPrice;
-        private static $nextIdBasket = 1;
+        private static $nextBasketID = 1; // Static property to count basket.
 
+        public function __construct(){
+            $this->_basketID += self::$nextBasketID; //Auto encrement of basket id. 
+            self::$nextBasketID ++; 
+        }
+
+        public function getArticles(){ return $this->_articles;}
+        public function getbasketID(){return $this->_basketID;}
+        public function getBasketWeight(){return $this->_basketWeight;}
+        public function getBasketPrice(){return $this->_basketPrice;}
+
+        public function addFruit(Fruit $fruits){  // add Fruit class in a addFruit method.
+            $this->_articles[] = $fruits; 
+        }
+        //Calculate total of basket weight in relation with each article weight. 
+        public function getTotalWeight(){
+            $total = 0;
+            foreach($this->_articles as $article){
+                $total += $article->getWeight(); 
+            }
+            return $total;
+        }
+        //Calculate total of basket price in relation with each article price.
+        public function getTotalPrice(){
+            $total = 0;
+            foreach($this->_articles as $article){
+                $total += $article->getPrice(); 
+            }
+            return $total;
+        }
+        // magic fonction to echo each instance of this class.
+        public function __toString(){
+            $showBasket = '<hr>';
+            $showBasket .= '<strong> Basket id: '. $this->_basketID.'</strong><br>';
+            foreach($this->_articles as $article){
+                $showBasket .= '<div class="left">'.$article.'</div>';
+            }
+            $showBasket .= '<div class="clearB">Total Weight : '. $this->getTotalWeight().' kg</div>'; 
+            $showBasket .= '<div class="clearB">Total Price : '. $this->getTotalPrice(). ' €</div>';
+            return $showBasket;
+        }
+        
     }
 ?>
